c1d3cf90f342333eed0bc6e3259ba3b5fa5f1138
Michael Coxe <mcoxe@me.com>
2021-03-26 13:47:10 -0800

    fixing a CI issue with Netlify

--- src/components/RichText.js
+++ src/components/RichText.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Box, Flex, Heading, Text, Center } from '@chakra-ui/react';
+import { Box, Heading, Text, Center } from '@chakra-ui/react';
 
 const RichText = ({ heading, text }) => {
 	return (
